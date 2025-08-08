0b2dd8e6036417dda9c83fa7aa011b63969084e0
Aiyzhy <mongush.aiyzhy@mail.ru>
2025-08-08 08:42:21 +0300

    KS-1-2 task 2 completed

---
+++ src/main/kotlin/lesson_2/task_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksText: String = "Thanks for the purchase"
+
+    println(numberOfOrders)
+    println(thanksText)
+
+    var numberOfWorkers: Int = 2000
+    //println(numberOfWorkers)
+
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+}
