7568ace5a14312046f6ebcbf4141b45e28eabe94
Aiyzhy <mongush.aiyzhy@mail.ru>
2025-08-08 10:30:08 +0300

    KS-1-4 error fixed

--- src/main/kotlin/lesson_1/task_4.kt
+++ src/main/kotlin/lesson_1/task_4.kt
@@ -4,7 +4,7 @@ fun main() {
     val length: Long = 40868600000L
     val age: Byte = 27
     val partOfDay: Float = 0.075f
-    val seconds: Int = 6480
+    val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
 
