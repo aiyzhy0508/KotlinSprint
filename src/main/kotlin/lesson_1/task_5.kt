df7a7cce8cf60ad7388c870268fd80495f229f82
Aiyzhy <mongush.aiyzhy@mail.ru>
2025-08-08 11:04:44 +0300

    KS-1-5 error fixed

--- src/main/kotlin/lesson_1/task_5.kt
+++ src/main/kotlin/lesson_1/task_5.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_1
 
 fun main() {
+    val time_60: Int = 60
     val seconds: Int = 6480
-    val minute: Int = seconds / 60
-    val reamainingSeconds: Int = seconds % 60
-    val hour: Int = minute / 60
-    val reamainingMinutes: Int = minute % 60
-    println("0$hour:$reamainingMinutes:0$reamainingSeconds")
+    val minute: Int = seconds / time_60
+    val reamainingSeconds: Int = seconds % time_60
+    val hour: Int = minute / time_60
+    val reamainingMinutes: Int = minute % time_60
+    println(String.format("%02d:%02d:%02d", hour, reamainingMinutes, reamainingSeconds))
 
 }
