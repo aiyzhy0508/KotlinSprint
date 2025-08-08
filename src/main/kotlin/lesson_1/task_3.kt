9c94928c3d0a4d8710eaa8fdfa8555d304375b92
Aiyzhy <mongush.aiyzhy@mail.ru>
2025-08-08 09:11:10 +0300

    KS-1-3 errors fixed

--- src/main/kotlin/lesson_1/task_3.kt
+++ src/main/kotlin/lesson_1/task_3.kt
@@ -1,17 +1,15 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
     println(hour)
     println(minute)
 
-    hour=10
-    minute=55
-    print(hour)
-    print(":")
-    print(minute)
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
 }
