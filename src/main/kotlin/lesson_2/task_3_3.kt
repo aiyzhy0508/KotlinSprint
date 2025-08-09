3787ed08c2c55a6fc0445c7079d07c58f5c748b0
Aiyzhy <mongush.aiyzhy@mail.ru>
2025-08-09 07:21:47 +0300

    KS-2-3 add new constant

--- src/main/kotlin/lesson_2/task_3_3.kt
+++ src/main/kotlin/lesson_2/task_3_3.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_2
 
 const val TIME_60 = 60
+const val COUNT_OF_MINUTES_IN_DAY = 1440
 fun main() {
     val exitHour = 9
     val exitMinute = 39
     val tripTime = 457
 
-    val arriveTimeInMinute = (exitHour * 60 + exitMinute + tripTime) % 1440
+    val arriveTimeInMinute = (exitHour * TIME_60 + exitMinute + tripTime) % COUNT_OF_MINUTES_IN_DAY
 
     val arriveHour = arriveTimeInMinute / TIME_60
     val arriveMinute = arriveTimeInMinute % TIME_60
